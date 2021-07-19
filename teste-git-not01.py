d92d058a2a3524be3020030f6a6d96541c793364
ThomazRossito <ThomazRossito>
2021-07-19 10:56:41 +0000

    update

--- teste-git-not01.py
+++ teste-git-not01.py
@@ -1,14 +1,3 @@
 # Databricks notebook source
-# Databricks notebook
-# Databricks notebook
+# Databricks versionamento notebook
 print('teste github')
-
-# COMMAND ----------
-
-print('teste github2')
-print('teste github3')
-
-# COMMAND ----------
-
-print('teste github2')
-print('teste github3')
